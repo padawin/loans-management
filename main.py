ea6dac9ace2495f32e0a014560ed12a8fb186463
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-04-08 22:40:38 +0200

    comments added

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python
 
+"""
+Entry point of the application
+"""
+
 import sys
 import loan
 import loanGUI
 
+
 def main(argv):
+	"""
+	Main method. Here are loaded the existing loans, and then the GUI is
+	created.
+	"""
 	l = loan.model.loadAll()
 	lApp = loanGUI.application(l, loan.model.fields)
 	sys.exit(lApp.run())
