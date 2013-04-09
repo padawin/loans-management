78972000d54f3fdb37eb0a6209cdf99d35ba434d
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-04-09 23:55:15 +0200

    do not execute values.sql if the file does not exist

--- database/createDb.sh
+++ database/createDb.sh
@@ -5,6 +5,8 @@ rm loan.db
 echo "creating db structure"
 sqlite3 -init structure.sql loan.db '.quit'
 
+[ ! -f values.sql ] && exit 0
+
 echo "inserting db values"
 sqlite3 -init values.sql loan.db '.quit'
 
