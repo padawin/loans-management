bd695e77d9d58ef342d4fb55f9689ef976d560fe
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-04-12 21:11:28 +0200

    class moved to add a property depending on model class

--- loan.py
+++ loan.py
@@ -3,13 +3,6 @@
 import Model
 
 
-class loan:
-	@staticmethod
-	def returnLoan(id):
-		import datetime
-		model.update({'date_return': datetime.date.today()}, ('id_loan = ?', [id]))
-
-
 class model(Model.Model):
 	fields = ['id_loan', 'lent_to', 'what', 'date_loan', 'date_return']
 
@@ -23,3 +16,12 @@ class model(Model.Model):
 			date_return IS NULL\
 		'
 		return Model.Model.fetchAllRows(query)
+
+
+class loan:
+	exportFields = [item for item in model.fields if item != 'id_loan']
+
+	@staticmethod
+	def returnLoan(id):
+		import datetime
+		model.update({'date_return': datetime.date.today()}, ('id_loan = ?', [id]))
