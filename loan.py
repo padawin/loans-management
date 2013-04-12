881118f17fbbf509cf7a18763ccf43c59268b155
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-04-12 20:48:40 +0200

    method added to list unreturned loans

--- loan.py
+++ loan.py
@@ -9,3 +9,14 @@ class loan:
 
 class model(Model.Model):
 	fields = ['id_loan', 'lent_to', 'what', 'date_loan', 'date_return']
+
+	@staticmethod
+	def loadUnreturned():
+		query = 'SELECT \
+			*\
+		FROM\
+			loan\
+		WHERE\
+			date_return IS NULL\
+		'
+		return Model.Model.fetchAllRows(query)
